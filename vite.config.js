c91581cff10dedb14d24bbb4c83c4f4cbdf5f719
Mitinul <torabi9030@gmail.com>
2025-01-27 23:11:46 +0330

    adding vite-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Automatically update the service worker
+      manifest: {
+        name: "Ghaza Chie? | غذا چیه",
+        short_name: "ghch | غذا چیه؟",
+        description: "مشاهده سریع وعدهٔ غذایی بعدی دانشگاه تهران.",
+        theme_color: "#EFE3C2",
+        background_color: "#3E7B27",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/vite.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/vite.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Enable PWA support during development for testing
+      },
+    }),
+  ],
+});
