1aef7175a216d715b472a957ff13aa74e325e432
Mitinul <torabi9030@gmail.com>
2025-01-27 23:38:18 +0330

    adding Header with h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { MealList } from "./components/MealList/MealList";
 import { NextMeals } from "./components/NextMeals/NextMeals";
+import { Header } from "./components/Header/Header";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Header />
       <NextMeals />
       <MealList />
     </QueryClientProvider>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+export function Header() {
+  return (
+    <div>
+      <h1
+        style={{
+          fontWeight: 700,
+          fontSize: 18,
+          margin: 25,
+          textAlign: "center",
+        }}
+      >
+        غذا چیه؟
+      </h1>
+    </div>
+  );
+}
