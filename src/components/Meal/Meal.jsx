24ffe685df1db848627fc1b6da8ce829aea22cac
Mitinul <torabi9030@gmail.com>
2025-01-28 09:09:31 +0330

    fixing style bug with overflow hidden

--- src/components/Meal/Meal.jsx
+++ src/components/Meal/Meal.jsx
@@ -10,7 +10,7 @@ export function Meal({ meal }) {
         color: "white",
         borderRadius: 5,
         backgroundColor: "#3E7B27",
-        fontWeight: today ? undefined : undefined,
+        overflow: "hidden",
       }}
     >
       <div
