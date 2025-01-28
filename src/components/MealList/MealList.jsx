a04546836531ba6c0f31a64ae351161df3293518
Mitinul <torabi9030@gmail.com>
2025-01-28 10:55:11 +0330

    styling MealList

--- src/components/MealList/MealList.jsx
+++ src/components/MealList/MealList.jsx
@@ -9,11 +9,30 @@ export function MealList() {
   if (isError) return <div>خطا در بارگیری اطلاعات.</div>;
 
   return (
-    <div>
-      <div>لیست غذا</div>
-      {meals.map((meal) => (
-        <Meal key={meal.date + meal.timeMeal} meal={meal} />
-      ))}
+    <div style={{ marginTop: 45, marginBottom: 20 }}>
+      <h2
+        style={{
+          fontWeight: 700,
+          fontSize: 18,
+          margin: 25,
+          textAlign: "center",
+        }}
+      >
+        برنامه غذایی
+      </h2>
+      <div
+        style={{
+          gap: 12,
+          display: "flex",
+          paddingLeft: 10,
+          paddingRight: 10,
+          flexDirection: "column",
+        }}
+      >
+        {meals.map((meal) => (
+          <Meal key={meal.date + meal.timeMeal} meal={meal} />
+        ))}
+      </div>
     </div>
   );
 }
