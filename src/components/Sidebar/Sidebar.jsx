b90b47b517c2c044bb93127ffee060dafec31c57
Mitinul <torabi9030@gmail.com>
2025-01-30 17:46:21 +0330

    updating sidebar size and padding and adding sidebar header

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -25,7 +25,7 @@ export function Sidebar({ isOpen, closeSidebar }) {
           top: 0,
           left: 0,
           backgroundColor: "#efe3c2",
-          width: "70vw",
+          width: "80vw",
           maxWidth: 500,
           height: "100vh",
           boxShadow: isOpen ? "2px 0px 8px #0004" : undefined,
@@ -33,16 +33,40 @@ export function Sidebar({ isOpen, closeSidebar }) {
           translate: isOpen ? undefined : "-100%",
           transition: ".3s ease-out",
         }}
+        dir="ltr"
       >
-        <a href="http://dining2.ut.ac.ir/" target="_blank">
-          <SidebarItem icon={ExternalLinkIcon}>
-            برو به سایت رزرو غذا
-          </SidebarItem>
-        </a>
-        <SidebarItem icon={UserIcon}>دربارهٔ ما</SidebarItem>
-        <a href="https://reymit.ir/ghaza" target="_blank">
-          <SidebarItem icon={DollarIcon}>حمایت مالی</SidebarItem>
-        </a>
+        <div
+          style={{
+            backgroundColor: "#123524",
+            color: "white",
+            padding: "30px 20px",
+            display: "flex",
+            gap: 25,
+          }}
+        >
+          <img
+            src="/ghch512.png"
+            alt="ghaza chie logo"
+            style={{ width: 50, height: 50, borderRadius: 999 }}
+          />
+          <div>
+            <div style={{ fontSize: 15, fontWeight: 600 }}>غذا چیه؟</div>
+            <div style={{ fontSize: 13, fontWeight: 400, opacity: 0.7 }}>
+              برنامهٔ غذایی دانشگاه تهران
+            </div>
+          </div>
+        </div>
+        <div style={{ padding: "15px 0" }}>
+          <a href="http://dining2.ut.ac.ir/" target="_blank">
+            <SidebarItem icon={ExternalLinkIcon}>
+              برو به سایت رزرو غذا
+            </SidebarItem>
+          </a>
+          <SidebarItem icon={UserIcon}>دربارهٔ ما</SidebarItem>
+          <a href="https://reymit.ir/ghaza" target="_blank">
+            <SidebarItem icon={DollarIcon}>حمایت مالی</SidebarItem>
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -58,7 +82,6 @@ function SidebarItem({ icon, children }) {
         cursor: "pointer",
         alignItems: "center",
       }}
-      dir="ltr"
     >
       <img src={icon} alt={children} />
       <div style={{ fontSize: 14, fontWeight: 600 }}>{children}</div>
