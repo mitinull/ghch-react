402919b4071367addd4f87f9bc601d18512c0f33
Mitinul <torabi9030@gmail.com>
2025-02-05 14:39:57 +0330

    updating Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import MenuIcon from "../../assets/menu.svg";
-import TwoLayersIcon from "../../assets/two-layers.svg";
-import ThreeLayersIcon from "../../assets/three-layers.svg";
+import LeftArrowIcon from "../../assets/left-arrow.svg";
 import { MEAL_LIST_TAB, NEXT_MEALS_TAB } from "../../constants";
 import { useTab } from "../../contexts/TabContext";
 import { useSidebar } from "../../contexts/SidebarContext";
@@ -15,38 +14,34 @@ export function Header() {
         margin: 25,
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
+        position: "relative",
       }}
     >
-      {tab === NEXT_MEALS_TAB && (
-        <img
-          src={TwoLayersIcon}
-          alt="menu"
-          onClick={() => setTab(MEAL_LIST_TAB)}
-        />
-      )}
-      {tab === MEAL_LIST_TAB && (
-        <img
-          src={ThreeLayersIcon}
-          alt="menu"
-          onClick={() => setTab(NEXT_MEALS_TAB)}
-        />
-      )}
       <h1
         style={{
           fontWeight: 700,
           fontSize: 18,
           textAlign: "center",
+          flex: 1,
         }}
       >
         {tab === NEXT_MEALS_TAB ? "غذا چیه؟" : "برنامهٔ غذایی"}
       </h1>
-      <img
-        src={MenuIcon}
-        alt="menu"
-        style={{ cursor: "pointer" }}
-        onClick={openSidebar}
-      />
+      {tab === NEXT_MEALS_TAB ? (
+        <img
+          src={MenuIcon}
+          alt="menu"
+          style={{ cursor: "pointer", position: "absolute", left: 0 }}
+          onClick={openSidebar}
+        />
+      ) : (
+        <img
+          src={LeftArrowIcon}
+          alt="back"
+          style={{ cursor: "pointer", position: "absolute", left: 0 }}
+          onClick={() => setTab(NEXT_MEALS_TAB)}
+        />
+      )}
     </div>
   );
 }
