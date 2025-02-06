39519f9e4686a6d90165f07d7cbd40f9a0bb3c61
Mitinul <torabi9030@gmail.com>
2025-02-06 15:51:34 +0330

    adding InstallTutorial page content

--- src/components/InstallTutorial/InstallTutorial.jsx
+++ src/components/InstallTutorial/InstallTutorial.jsx
@@ -4,6 +4,19 @@ export function InstallTutorial() {
   return (
     <div>
       <Header title="آموزش نصب" />
+      <div
+        style={{ fontWeight: 300, padding: "0 25px", fontSize: 18 }}
+        className="page-enter"
+      >
+        روی <Bold>سه‌نقطهٔ</Bold> بالای مرورگر بزن و{" "}
+        <Bold>«افزودن به صفحهٔ اصلی»</Bold> یا <Bold>«نصب برنامه»</Bold> رو
+        انتخاب کن. بعدش <Bold>«غذا چیه؟»</Bold> میاد رو صفحهٔ اصلیت و مثل بقیهٔ
+        اپ‌ها همیشه دم دستته! حتی بدون اینترنت هم می‌تونی ازش استفاده کنی! 😎🚀
+      </div>
     </div>
   );
+}
+
+function Bold({ children }) {
+  return <span style={{ fontWeight: 500 }}>{children}</span>;
 }
